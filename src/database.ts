9f6e6836716d3a14b4db1eed6becbe041cb0fc56
Bahjet <bah.jet@hotmail.com>
2024-07-16 17:52:23 -0300

    configuração produção

--- src/database.ts
+++ src/database.ts
@@ -3,10 +3,12 @@ import { env } from './env'
 // npm run knex -- migrate:make create-transactions criando migration
 
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  // conection pode ser padrão sqlite ou pg
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? { filename: env.DATABASE_URL }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
--- src/env/index.ts
+++ src/env/index.ts
@@ -9,9 +9,10 @@ if (process.env.NODE_ENV === 'test') {
 }
 
 const envSchema = z.object({
-  DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
+  DATABASE_URL: z.string(),
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
