c3ce7d63131892bcdba6df5ef523af99274e745c
Bahjet <bah.jet@hotmail.com>
2024-07-16 19:44:25 -0300

    host

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,9 @@ import { env } from './env'
 
 app
   .listen({
+    host: '0.0.0.0',
     port: env.PORT,
   })
   .then(() => {
-    console.log('HTTP Server Running na porta: ' + env.PORT)
+    console.log('HTTP Server Running!')
   })
